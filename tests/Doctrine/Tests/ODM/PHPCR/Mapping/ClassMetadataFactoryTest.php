6cd9b1f2ae178ede2bd51cc2cddc39ca3e4d1555
dantleech <daniel@ylyad.fr>
2012-11-01 12:27:20 +0100

    Added getAnnotationDriver method

--- tests/Doctrine/Tests/ODM/PHPCR/Mapping/ClassMetadataFactoryTest.php
+++ tests/Doctrine/Tests/ODM/PHPCR/Mapping/ClassMetadataFactoryTest.php
@@ -11,6 +11,16 @@ class ClassMetadataFactoryTest extends \PHPUnit_Framework_TestCase
      */
     private $dm;
 
+    protected function getAnnotationDriver()
+    {
+        $cache = new \Doctrine\Common\Cache\ArrayCache();
+        $reader = new \Doctrine\Common\Annotations\AnnotationReader($cache);
+        $annotationDriver = new \Doctrine\ODM\PHPCR\Mapping\Driver\AnnotationDriver($reader);
+        $annotationDriver->addPaths(array(__DIR__ . '/Model'));
+
+        return $annotationDriver;
+    }
+
     public function setUp()
     {
         $session = $this->getMock('PHPCR\SessionInterface');
@@ -62,11 +72,8 @@ class ClassMetadataFactoryTest extends \PHPUnit_Framework_TestCase
         // if the child class overrides referenceable as false it is not taken into account
         // as we only ever set the referenceable property to TRUE. This prevents us from
         // knowing if the user has explicitly set referenceable to FALSE on a child entity.
-        
-        $cache = new \Doctrine\Common\Cache\ArrayCache();
-        $reader = new \Doctrine\Common\Annotations\AnnotationReader($cache);
-        $annotationDriver = new \Doctrine\ODM\PHPCR\Mapping\Driver\AnnotationDriver($reader);
-        $annotationDriver->addPaths(array(__DIR__ . '/Model'));
+
+        $annotationDriver = $this->getAnnotationDriver();
         $this->dm->getConfiguration()->setMetadataDriverImpl($annotationDriver);
 
         $cmf = new ClassMetadataFactory($this->dm);
@@ -77,10 +84,7 @@ class ClassMetadataFactoryTest extends \PHPUnit_Framework_TestCase
 
     public function testLoadMetadata_referenceableChild()
     {
-        $cache = new \Doctrine\Common\Cache\ArrayCache();
-        $reader = new \Doctrine\Common\Annotations\AnnotationReader($cache);
-        $annotationDriver = new \Doctrine\ODM\PHPCR\Mapping\Driver\AnnotationDriver($reader);
-        $annotationDriver->addPaths(array(__DIR__ . '/Model'));
+        $annotationDriver = $this->getAnnotationDriver();
         $this->dm->getConfiguration()->setMetadataDriverImpl($annotationDriver);
 
         $cmf = new ClassMetadataFactory($this->dm);
